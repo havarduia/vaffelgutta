e44d8c456f271ecc92d45507add273fa568c1c7b
Vaffel-desktop <andreas.c.thors1@gmail.com>
2025-01-23 17:27:48 +0100

    Made a setup.sh file for building library for camera

--- camera/run_nodes.sh
+++ camera/run_nodes.sh
@@ -2,8 +2,8 @@
 
 #Virtual Workspace camera
 gnome-terminal -- bash -c "
-source ~/git/vaffelgutta/virtual/camera_env/bin/activate &&
-source ~/git/vaffelgutta/camera_library/camera_ws/install/setup.bash &&
+source ~/git/vaffelgutta/camera/camera_env/bin/activate &&
+source ~/git/vaffelgutta/camera/camera_library/camera_ws/install/setup.bash &&
 ros2 launch aruco_pose_estimation aruco_pose_estimation.launch.py;
 exec bash"
 

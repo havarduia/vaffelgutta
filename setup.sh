689b4bf6d8f2caae28da2ee2906df98d8138abc4
Vaffel-desktop <andreas.c.thors1@gmail.com>
2025-03-11 18:44:45 +0100

    reworked setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get upgrade -y
-# install python requirements
-sudo apt install python3 -y
-pip install -r requirements.txt
+current_dir=$(pwd)
+
 # set python path
 if echo "$USER" | grep -q "vaffelgutta" 
     then
     validinput=0
     while ! [[ $validinput == 1 ]]
     do
-        echo "Are you running this script for the first time? y/n"
+        sudo echo "Are you running this script for the first time? y/n"
         read myinput
-        if [ $myinput == "y" ] || [ $myinput == "n" ]
+        if [[ $myinput == "y" ]] || [[ $myinput == "n" ]]
         then
             validinput=1 
             if [ $myinput == "y" ]
@@ -31,10 +28,38 @@ elif echo "$PYTHONPATH" | grep -q "vaffelgutta"
 else
     append_path=true
 fi
-if [[ $append_path == true ]]
+if [[ $append_path == "true" ]]
 then
-    echo "export PYTHONPATH=$PYTHONPATH:$PWD" >> ~/.bashrc 
+    echo "export PYTHONPATH=$PYTHONPATH:$current_dir" >> ~/.bashrc 
 fi
+# install interbotix library
+cd 
+# neccesary files for moveit and gazebo respectively
+echo 'export LC_NUMERIC="en_US.UTF-8"' >> ~/.bashrc
+echo 'export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-11:' >> ~/.bashrc
+sudo apt install curl -y
+sudo apt update && sudo apt install -y dbus-x11
+architecture=$(dpkg --print-architecture)
+TERMINAL=$(command -v gnome-terminal || command -v konsole || command -v x-terminal-emulator || command -v xterm || command -v lxterminal || echo "")
+
+if [[ $architecture == "arm64" ]]
+then
+    curl 'https://raw.githubusercontent.com/Interbotix/interbotix_ros_manipulators/main/interbotix_ros_xsarms/install/rpi4/xsarm_rpi4_install.sh' > xsarm_rpi4_install.sh
+    chmod +x xsarm_rpi4_install.sh 
+else
+    curl 'https://raw.githubusercontent.com/Interbotix/interbotix_ros_manipulators/main/interbotix_ros_xsarms/install/amd64/xsarm_amd64_install.sh' > xsarm_amd64_install.sh
+    chmod +x xsarm_amd64_install.sh
+fi
+
+# install
+sudo apt-get update -y
+sudo apt-get upgrade -y
+# install python requirements
+cd $current_dir 
+
+sudo apt install python3 -y
+pip install -r requirements.txt
+
 
 
 # realsense
@@ -50,25 +75,3 @@ cd build
 cmake ../ -DFORCE_RSUSB_BACKEND=true -DBUILD_PYTHON_BINDINGS:bool=true -DPYTHON_EXECUTABLE=$(which python3) 
 make -j$(nproc)
 sudo make install -j$(nproc)
-
-# install interbotix library
-cd 
-# neccesary files for moveit and gazebo respectively
-echo 'export LC_NUMERIC="en_US.UTF-8"' >> ~/.bashrc
-echo 'export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-11:' >> ~/.bashrc
-sudo apt install curl
-
-architecture=$(dpkg --print-architecture)
-architecture="arm64"
-if [[ architecture == "arm64" ]]
-then
-    curl 'https://raw.githubusercontent.com/Interbotix/interbotix_ros_manipulators/main/interbotix_ros_xsarms/install/rpi4/xsarm_rpi4_install.sh' > xsarm_rpi4_install.sh
-    chmod +x xsarm_rpi4_install.sh
-    sudo ./xsarm_rpi4_install.sh -d humble -n
-else
-    curl 'https://raw.githubusercontent.com/Interbotix/interbotix_ros_manipulators/main/interbotix_ros_xsarms/install/amd64/xsarm_amd64_install.sh' > xsarm_amd64_install.sh
-    chmod +x xsarm_amd64_install.sh
-    sudo ./xsarm_amd64_install.sh -d humble -n
-fi
-
-sudo reboot

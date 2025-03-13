d12a98c42f1ea1c9bbf71ba78bda496e9e5977d3
Vaffel-desktop <andreas.c.thors1@gmail.com>
2025-03-13 17:00:45 +0100

    added copy camera to test robot offset movements

--- test_Projects/copy_camera.py
+++ test_Projects/copy_camera.py
@@ -1,20 +1,44 @@
 # Change the working directory to the base directory
-from os import chdir
-from os import path as ospath 
-from sys import path as syspath
-chdir(ospath.expanduser("~/git/vaffelgutta"))
-syspath.append(ospath.abspath(ospath.expanduser("~/git/vaffelgutta")))
-from threading import Thread
-from robot.robot_controllers.Wafflebot import Wafflebot
-from robot.executable_scripts.common.errorhandling import handle_error
+from robot.robot_controllers.Wafflebot.Wafflebot import Wafflebot
+from robot.tools.errorhandling import handle_error
 from time import sleep
 from robot.tools.file_manipulation import Jsonreader
 from robot.tools.visualizers.tf_publisher import TFPublisher
+from robot.tools.update_tagoffsets import create_offset_matrix, abs_position_from_offset
+
+
 def get_aruco_pose(id: str):
     reader = Jsonreader()
     tags = reader.read("camera_readings")        
     return tags.get(id)
 
+def recordOffset(bot: Wafflebot, tagid: str):
+    bot_pos = bot.arm.get_ee_pose()
+    reader = Jsonreader()
+    tag_pos = reader.read("camera_readings")[tagid]
+    offset = create_offset_matrix(bot_pos, tag_pos)
+    reader.write("offsets", {"copy_camera": offset}) 
+
+def goToTag(bot: Wafflebot, tagid:str, visualizer: TFPublisher = None ):
+    reader = Jsonreader()
+    tag_pos = reader.read("camera_readings")[tagid]
+    offset = reader.read("offsets")["copy_camera"]
+    target = abs_position_from_offset(tag_pos, offset)
+    # plan a:
+    bot.move(target)
+    # plan b:
+    """
+    visualizer.broadcast_transform(target)
+    bot.arm.set_ee_pose_matrix(target, blocking=False)
+    """
+
+def printmenu():
+    print("Press 1 to record offset")
+    print("Press 2 to go to offset")
+    print("press 3 to set tag id")
+    print("Press 4 to toggle arm torque")
+    print("press 5 to exit")
+    return
 
 def main():
     # Init robot
@@ -22,15 +46,33 @@ def main():
     bot.arm.go_to_home_pose()
     pub = TFPublisher()
     # Put your code here:
-    running = True
-    while running:
+    tagid = "25"
+    torqed = True
+    while True:
+        printmenu()
+        choice = input("Input: ")
         try:
-            pose = get_aruco_pose("25")
-            pub.broadcast_transform(pose)
-            bot.move(pose)
-            sleep(0.1)
-        except KeyboardInterrupt:
-            running = False
+            choice = int(choice)
+        except ValueError:
+            print("That was not a number😡") # 😡
+        match choice:
+            case 1:
+                recordOffset(bot, tagid)
+            case 2:
+                goToTag(bot, tagid, pub)
+            case 3: 
+                tagid = str(input("Input new ID: "))
+            case 4:
+                bot.core.robot_torque_enable("group", "arm", not torqed)
+                torqed = not torqed
+            case 5:
+                break
+            case _:
+                print("invalid input. Try again.")
+    
+    
+    
+    
     # Close bot, close program:
     bot.safe_stop()
 

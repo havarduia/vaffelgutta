403ddf7369ce8c5cf9d314f37a3f3ecd3c9056e7
Håvard Karlsen Solheim <haavardkso@uia.no>
2025-03-13 07:01:15 +0100

    Fixed an error where it wouldnt find config.yaml

--- camera/camera_config_loader.py
+++ camera/camera_config_loader.py
@@ -1,7 +1,7 @@
 import yaml
-
+import os
 class ConfigLoader:
-    def __init__(self, config_path="config.yaml"):
+    def __init__(self, config_path=os.path.expanduser('~/git/vaffelgutta/camera/config.yaml')):
         with open(config_path, 'r') as file:
             self.config = yaml.safe_load(file)
     

a3f7e52752803bc3676dee53e1ab7a344eae6454
Vaffel-desktop <andreas.c.thors1@gmail.com>
2025-03-26 15:27:50 +0100

    added experimental LERPing home/sleep pose functions

--- robot/robot_controllers/Wafflebot/safe_stop.py
+++ robot/robot_controllers/Wafflebot/safe_stop.py
@@ -1,7 +1,13 @@
 from time import sleep
 def safe_stop(bot:"Wafflebot", slow):
-    bot.arm.set_trajectory_time(moving_time=(8.0 if slow else 2.0)) # reset moving time if changed elsewhere
+    home_pose = bot.arm.robot_des.M
+    sleep_pose = (
+        [[ 0.83094068, 0.,          0.55636102,  0.13459297],
+        [ 0.,          1.,          0.,          0.,        ],
+        [-0.55636102,  0.,          0.83094068,  0.09320746],
+        [ 0.,          0.,          0.,          1.        ]]) 
     bot.core.robot_torque_enable("group", "arm", True)
+    bot.arm.set_trajectory_time(moving_time=(8.0 if slow else 2.0)) # reset moving time if changed elsewhere
     sleep(2)
     bot.arm.go_to_home_pose()
     sleep_joints =  [0.0, -1.80, 1.6, 0.0, 0.5859, 0.0]

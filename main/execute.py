1e7a61ef144fb34b5c444a6c39abb039834c1621
Håvard Karlsen Solheim <haavardkso@uia.no>
2025-03-28 16:51:48 +0100

    Added skeleton functions for the state machine

--- main/execute.py
+++ main/execute.py
@@ -1,4 +1,4 @@
-from robot.tools.waffle_states.waffle_states import State, CurrentState
+from main.waffle_states.waffle_states import State, CurrentState
 
 from main.state_transitions import *
 
@@ -13,27 +13,90 @@ def execute(
     """
 
     state = CurrentState()
-
-    match state.get_state():    
-        
+    current_state = state.get()
+   
+    match current_state:    
         case State.SLEEP:
-            # named sleepstate instead of sleep to avoid import conflict
-            sleepstate(state, bot) 
+            sleepstate(state, bot)
+             
         case State.REST:
             print("I am resting, dammit. These youngins...")
             rest(state, bot)
         
-        case State.state1:
-            print("I am a demo state!")
+        case State.HOME:
+            print("I am going home, long day today.")
+            home(state, bot)
+
+        case State.OPEN_IRON:
+            print("Opening iron!")
+            open_iron(state, bot)
+            
+        case State.PICK_UP_SPRAY:
+            print("Picking up spray 👺")
+            pick_up_spray(state, bot)
+            
+        case State.SPRAY:
+            print("Im spraying AHHHH!")
+            spray(state, bot)       
+        
+        case State.PUT_DOWN_SPRAY:
+            print("Sorry i dont have anything left...")
+            put_down_spray(state, bot)
+            
+        case State.PLACE_CUP_TO_STAT:
+            print("Give me the cup.")
+            place_cup_to_stat(state, bot)
+            
+        case State.FILL_CUP:
+            print("Im filling AHHH!")
+            fill_cup(state, bot)       
+        
+        case State.CUP_TO_IRON:
+            print("Batter to iron!")
+            cup_to_iron(state, bot)
+            
+        case State.EMPTY_CUP:
+            print("im emptying my cup AHHH!")
+            empty_cup(state, bot)
+            
+        case State.RETURN_CUP:
+            print("Lets put the cup back.")
+            return_cup(state, bot)
+        
+        case State.CLOSE_IRON:
+            print("Its all going well, closing iron!")
+            close_iron(state, bot)
+            
+        case State.FUN_TIME:
+            print("This is where the fun begins.")
+            fun_time(state, bot)
+            
+        case State.OPEN_IRON2:
+            print("Waffle is done opening iron!")
+            open_iron2(state, bot)
+            
+        case State.PICK_UP_WAFFLE:
+            print("Look at my child, its beautiful")
+            pick_up_waffle(state, bot)
+            
+        case State.SERVE_WAFFLE:
+            print("Here is your waffle kind sir 🤵")
+            serve_waffle(state, bot)
+            
+        case State.RETURN_STICK:
+            print("Stick to IRON!!!")
+            return_stick(state, bot)
+            
+        case State.ERROR:
+            print("Hagle")
+            error(state, bot)
 
         case _:
-            bot.safe_stop(slow = True) 
+            bot.safe_stop(slow=True) 
             print("An unknown state was encountered!")
             return False
+
     return True
-
-
-
 
 if __name__ == "__main__":
     # For type hinting:
--- main/waffle_states/waffle_states.py
+++ main/waffle_states/waffle_states.py
@@ -1,11 +1,25 @@
 from enum import Enum
 
 class State(Enum):
-    REST = 0,
-    HOME = 1,
-    SLEEP = 2,
-    state1 = 3,
-    state2 = 4,
+    REST = 1
+    HOME = 2
+    SLEEP = 3
+    OPEN_IRON = 4 
+    PICK_UP_SPRAY = 5
+    SPRAY = 6
+    PUT_DOWN_SPRAY = 7
+    PLACE_CUP_TO_STAT = 8
+    FILL_CUP = 9
+    CUP_TO_IRON = 10
+    EMPTY_CUP = 11
+    RETURN_CUP = 12
+    CLOSE_IRON = 13
+    FUN_TIME = 14
+    OPEN_IRON2 = 15
+    PICK_UP_WAFFLE = 16
+    SERVE_WAFFLE = 17
+    RETURN_STICK = 18
+    ERROR = 666
     #...fill in states here
 
 

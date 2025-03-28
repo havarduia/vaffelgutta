9dc321d67bb8701f9394c72840945b66d6890b2b
Håvard Karlsen Solheim <haavardkso@uia.no>
2025-03-28 16:51:48 +0100

    Added skeleton functions for the state machine

--- main/main.py
+++ main/main.py
@@ -6,7 +6,7 @@ It should however be viewed as a template and not funtional
 from camera.init_camera import initalize_system as init_camera
 from robot.tools.errorhandling import handle_error
 from robot.robot_controllers.Wafflebot import Wafflebot
-from robot.tools.waffle_states.waffle_states import CurrentState
+from waffle_states import State, CurrentState
 from main.execute import execute
 
 def init():

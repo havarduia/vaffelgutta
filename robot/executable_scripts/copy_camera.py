e7ccccb178988f375c74b78d9e8429b4d45ab76d
Vaffel-desktop <andreas.c.thors1@gmail.com>
2025-04-16 17:51:23 +0200

    added DU v1

--- robot/executable_scripts/copy_camera.py
+++ robot/executable_scripts/copy_camera.py
@@ -1,4 +1,5 @@
 from robot.robot_controllers.Wafflebot.Wafflebot import Wafflebot
+from robot.robot_controllers.path_planner import list_multiply, list_sum
 from robot.tools.errorhandling import handle_error
 from rclpy._rclpy_pybind11 import RCLError
 from time import sleep, time
@@ -11,6 +12,7 @@ from threading import Thread
 import cv2
 from robot.tools.timekeeper import record_time, read_times
 import os
+import numpy as numphy
 
 
 def get_aruco_pose(id: str):
@@ -27,9 +29,44 @@ def recordOffset(bot: Wafflebot, tagid: str, visualizer: TFPublisher = None):
     reader.write("offsets", {"copy_camera": offset})
     print(f"written offset to copy_camera")
 
-    return
+def follow_DU(bot: Wafflebot, tagid):
+    distance = 0.2 
+    tol = 0.01
+    arm_height = 0.42705
+    rTarget = 0.4
+    offset =[
+    [0.0,0.0,1.0,0.0],
+    [0.0,1.0,0.0,0.0],
+    [-1.0,0.0,0.0,0.20],
+    [0.0,0.0,0.0,1.0]
+    ]
+    reader = Jsonreader()
+    starttime = time()
+    endtime = time()
+    while endtime - starttime <= 10:
+        endtime = time()
+        bot.vision.run_once()
+        tag_pos = reader.read("camera_readings")[tagid]
+        target = numphy.array(abs_position_from_offset(tag_pos, offset))
+        pos = target[3,0:3]
+        
+        pos[3] -= arm_height
+        r = numphy.sqrt(pos[0]**2 + pos[1]**2 + pos[2]**2)
+        pos = pos * rTarget / r
+        pos[2] += arm_height
+        print("Moving robot")
+        # plan a:
+        print(f"movement success? {
+              bot.move(target, speed_scaling=4.0)
+              }")
+        
+        
 
 
+            
+
+
+        
 def goToTag(bot: Wafflebot, tagid:str, pre_offset):
     i = 0
     reader = Jsonreader()
@@ -37,9 +74,7 @@ def goToTag(bot: Wafflebot, tagid:str, pre_offset):
     endtime = time()
     while endtime - starttime <= 10:
         endtime = time()
-        record_time("(buffer)")
-        bot.vision.run_once("all")
-        record_time(f"camera_frame_no_{i}")
+        bot.vision.run_once()
         tag_pos = reader.read("camera_readings")[tagid]
         if pre_offset is not None:
             offset = pre_offset
@@ -50,7 +85,6 @@ def goToTag(bot: Wafflebot, tagid:str, pre_offset):
         print("Moving robot")
         # plan a:
         print(f"movement success? {bot.move(target, speed_scaling=4.0)}")
-        record_time(f"robot_frame_no_{i}")
         # plan b:
         #bot.arm.set_ee_pose_matrix(target, blocking=False)
 
@@ -73,6 +107,7 @@ def printmenu():
     print("Press 4 to toggle arm torque")
     print("press 5 to exit")
     print("press 6 to follow the tag like a silly lil goose")
+    print("press HAGLE to follow DU")
     return 
 
 def main(bot):
@@ -112,13 +147,9 @@ def main(bot):
             case 5:
                 break
             case 6:
-                os.system("clear")
-                record_time("start")
-                record_time(f"move_start_{movement_number}")
                 follow_tag(bot,tagid)
-                record_time(f"move_end_{movement_number}")
-                movement_number +=1
-                read_times()
+            case 41453:
+                follow_DU(bot, tagid)
             case _:
                 print("invalid input. Try again.")
 
